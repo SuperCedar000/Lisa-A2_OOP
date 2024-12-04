82f1ded3fa9f1d6318367979e67770580726279a
SuperCedar000 <775255837@qq.com>
2024-12-04 17:28:59 +0800

    update
    The Ride class implements the RideInterface interface, manages visitor queues and history, and provides complete code for import and export functions

--- Ride.java
+++ Ride.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 public class Ride implements RideInterface {
@@ -14,4 +13,36 @@ public class Ride implements RideInterface {
         this.history = new ArrayList<>();
     }
 
-   
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        queue.add(visitor);
+        System.out.println(visitor.getName() + " added to the queue.");
+    }
+
+    @Override
+    public void runOneCycle() {
+        System.out.println("Running one cycle of the ride: " + name);
+        int riders = Math.min(queue.size(), maxRiders);
+        for (int i = 0; i < riders; i++) {
+            Visitor visitor = queue.poll(); // 从队列中移除
+            history.add(visitor);          // 添加到历史记录
+            System.out.println(visitor.getName() + " enjoyed the ride.");
+        }
+    }
+
+    @Override
+    public void printQueue() {
+        System.out.println("Current queue for " + name + ":");
+        for (Visitor visitor : queue) {
+            System.out.println("- " + visitor.getName());
+        }
+    }
+
+    @Override
+    public void printRideHistory() {
+        System.out.println("Ride history for " + name + ":");
+        for (Visitor visitor : history) {
+            System.out.println("- " + visitor.getName());
+        }
+    }
+}
