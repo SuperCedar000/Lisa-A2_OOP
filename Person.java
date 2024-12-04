7b0268081241a906382b6d55f27f2261db8be5e0
SuperCedar000 <775255837@qq.com>
2024-12-04 17:19:25 +0800

    Initial commit

---
+++ Person.java
@@ -0,0 +1,20 @@
+public abstract class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // 抽象方法，要求子类实现
+    public abstract void displayInfo();
+}
