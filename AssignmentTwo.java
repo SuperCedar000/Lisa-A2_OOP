d409190bbc01aba202af0635af1d7c352ae28d4a
SuperCedar000 <775255837@qq.com>
2024-12-04 17:32:03 +0800

    update(fix)
    Modify the main class of the provider, perform the operation of the ride, import and export operations, and show the code how to use these functions

--- AssignmentTwo.java
+++ AssignmentTwo.java
@@ -1,5 +1,3 @@
-import java.io.File;
-
 public class AssignmentTwo {
 
     public static void main(String[] args) {
@@ -13,19 +11,4 @@ public class AssignmentTwo {
         Visitor visitor4 = new Visitor("Diana Prince", 30, "VIP004", false);
         Visitor visitor5 = new Visitor("Ethan Clark", 40, "VIP005", true);
 
-        // 添加游客到 Ride
-        rollerCoaster.addVisitorToQueue(visitor1);
-        rollerCoaster.addVisitorToQueue(visitor2);
-        rollerCoaster.addVisitorToQueue(visitor3);
-        rollerCoaster.addVisitorToQueue(visitor4);
-        rollerCoaster.addVisitorToQueue(visitor5);
-
-        // 运行一次游乐设施
-        rollerCoaster.runOneCycle();
-
-        // 导出游客历史记录到文件
-        String filename = "ride_history.txt";
-        rollerCoaster.exportRideHistory(filename);
-
-        // 在 partSeven 方法中进行导入
-        partSeven(rollerCo
+     
